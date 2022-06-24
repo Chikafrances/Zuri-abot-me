1a6228a4a13b215ba63ef1716b5653960d6c267b
Chikafrances <chikafrances1@gmail.com>
2022-06-24 14:48:31 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+alert('My name is Chika');
+alert('5.9ft');
+alert('Nigeria');
+
+var name = ('My name is Chika Akwue');
+var height = ('5.9ft');
+var country = ('Nigeria');
+
+console.log(name);
+console.log(height);
+console.log(country);
